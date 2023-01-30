a6068dbf334cb49224530fae0255d620a9c4b8d7
PK-Yakkun <mokkun515@gmail.com>
2023-01-31 00:09:08 +0900

    add: pre-commitとrollupの設定ファイル追加

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-const pkg = require("./package.json");
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: pkg.main,
+        file: "dist/cjs/index.js",
         format: "cjs",
         sourcemap: false,
       },
       {
-        file: pkg.module,
+        file: "dist/esm/index.js",
         format: "esm",
         sourcemap: false,
       },
